913cd95ec362e77fd5de44442cc881d81b8dfe35
RlyTachi <rlytachi@yandex.ru>
2020-10-21 02:02:12 +0300

    game ui update
    Signed-off-by: RlyTachi <rlytachi@yandex.ru>

--- app/src/main/java/com/rlytachi/animequiz/Game.java
+++ app/src/main/java/com/rlytachi/animequiz/Game.java
@@ -2,13 +2,35 @@ package com.rlytachi.animequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class Game extends AppCompatActivity {
+public class Game extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        final ImageView back = (ImageView) findViewById(R.id.backView);
+        final ImageView unlock = (ImageView) findViewById(R.id.unlockView);
+        back.setOnClickListener(this);
+        unlock.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.backView:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.unlockView:
+                startActivity(new Intent(this, Unlock.class));
+                break;
+
+        }
     }
 }

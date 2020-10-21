a087df726025bde1a9ff09fbc6929d512d0bc486
RlyTachi <rlytachi@ya.ru>
2020-10-22 01:01:58 +0300

    first level ui added
    Signed-off-by: RlyTachi <rlytachi@ya.ru>

--- app/src/main/java/com/rlytachi/animequiz/Game.java
+++ app/src/main/java/com/rlytachi/animequiz/Game.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.rlytachi.animequiz.levels.Characters;
 
 public class Game extends AppCompatActivity implements View.OnClickListener {
 
@@ -14,11 +17,25 @@ public class Game extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        //Layouts
+        final LinearLayout characters = (LinearLayout) findViewById(R.id.charactersLayout);
+        final LinearLayout locations = (LinearLayout) findViewById(R.id.locationsLayout);
+        final LinearLayout titles = (LinearLayout) findViewById(R.id.titlesLayout);
+        final LinearLayout items = (LinearLayout) findViewById(R.id.itemsLayout);
+        final LinearLayout events = (LinearLayout) findViewById(R.id.eventsLayout);
+
+        //Bottom buttons
         final ImageView back = (ImageView) findViewById(R.id.backView);
         final ImageView unlock = (ImageView) findViewById(R.id.unlockView);
+
+        //Listeners
+        characters.setOnClickListener(this);
+        locations.setOnClickListener(this);
+        titles.setOnClickListener(this);
+        items.setOnClickListener(this);
+        events.setOnClickListener(this);
         back.setOnClickListener(this);
         unlock.setOnClickListener(this);
-
     }
 
     @Override
@@ -29,6 +46,9 @@ public class Game extends AppCompatActivity implements View.OnClickListener {
                 break;
             case R.id.unlockView:
                 startActivity(new Intent(this, Unlock.class));
+                break;
+            case R.id.charactersLayout:
+                startActivity(new Intent(this, Characters.class));
                 break;
 
         }
---
+++ app/src/main/java/com/rlytachi/animequiz/levels/Characters.java
@@ -0,0 +1,16 @@
+package com.rlytachi.animequiz.levels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rlytachi.animequiz.R;
+
+public class Characters extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_characters);
+    }
+}

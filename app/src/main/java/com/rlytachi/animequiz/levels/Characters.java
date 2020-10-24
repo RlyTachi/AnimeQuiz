d66e191acb5a4a78c8031e8efd8c12624f73f036
RlyTachi <rlytachi@ya.ru>
2020-10-24 05:18:20 +0300

    refactored full class
    Signed-off-by: RlyTachi <rlytachi@ya.ru>

--- app/src/main/java/com/rlytachi/animequiz/levels/Characters.java
+++ app/src/main/java/com/rlytachi/animequiz/levels/Characters.java
@@ -2,6 +2,7 @@ package com.rlytachi.animequiz.levels;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
@@ -14,23 +15,18 @@ import android.widget.TextView;
 import com.rlytachi.animequiz.Game;
 import com.rlytachi.animequiz.R;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Set;
 
 public class Characters extends AppCompatActivity implements View.OnClickListener {
 
-    Thread failure = new Failure();
-    HashMap<Integer, String> questions = new HashMap<>();
+    LinkedList<String> questions = new LinkedList<>();
     LinkedList<Drawable> images = new LinkedList<>();
-    boolean correctAnswer = false;
     Integer[] imageArrCount;
     int image = 0;
-
+    int heartCount = 3;
+    int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +34,14 @@ public class Characters extends AppCompatActivity implements View.OnClickListene
         setContentView(R.layout.activity_characters);
 
         //Images
-        final ImageView backView = (ImageView) findViewById(R.id.backView);
-        final ImageView helpView = (ImageView) findViewById(R.id.helpView);
+        final ImageView backView = findViewById(R.id.backView);
+        final ImageView helpView = findViewById(R.id.helpView);
         //Buttons
-        final Button btn1 = (Button) findViewById(R.id.btnCh1);
-        final Button btn2 = (Button) findViewById(R.id.btnCh2);
-        final Button btn3 = (Button) findViewById(R.id.btnCh3);
-        final Button btn4 = (Button) findViewById(R.id.btnCh4);
-        final TextView info = (TextView) findViewById(R.id.informationText);
-
+        final Button btn1 = findViewById(R.id.btnCh1);
+        final Button btn2 = findViewById(R.id.btnCh2);
+        final Button btn3 = findViewById(R.id.btnCh3);
+        final Button btn4 = findViewById(R.id.btnCh4);
+        final TextView info = findViewById(R.id.informationText);
 
         //Click listener
         backView.setOnClickListener(this);
@@ -56,18 +51,21 @@ public class Characters extends AppCompatActivity implements View.OnClickListene
         btn3.setOnClickListener(this);
         btn4.setOnClickListener(this);
 
-        //HashMap
-        questions.put(0, "Бака");
-        questions.put(1, "Не бака");
-        questions.put(2, "Фосфофиллит");
-        questions.put(3, "Фосссс");
-
         //Linked list
+        questions.add("Первый");
+        questions.add("Второй");
+        questions.add("Третий");
+        questions.add("Четвертый");
+        questions.add("Пятый");
+        questions.add("Шестой");
+        questions.add("Седьмой");
         images.add(getDrawable(R.drawable.ch1));
         images.add(getDrawable(R.drawable.ch2));
         images.add(getDrawable(R.drawable.ch3));
         images.add(getDrawable(R.drawable.ch4));
-
+        images.add(getDrawable(R.drawable.heart));
+        images.add(getDrawable(R.drawable.play));
+        images.add(getDrawable(R.drawable.emilia));
 
     }
 
@@ -77,16 +75,16 @@ public class Characters extends AppCompatActivity implements View.OnClickListene
 
         imageArrCount = getRandomArray();
         nextQuestion();
-        failure.start();
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
         //Buttons
-        final Button btn1 = (Button) findViewById(R.id.btnCh1);
-        final Button btn2 = (Button) findViewById(R.id.btnCh2);
-        final Button btn3 = (Button) findViewById(R.id.btnCh3);
-        final Button btn4 = (Button) findViewById(R.id.btnCh4);
+        final Button btn1 = findViewById(R.id.btnCh1);
+        final Button btn2 = findViewById(R.id.btnCh2);
+        final Button btn3 = findViewById(R.id.btnCh3);
+        final Button btn4 = findViewById(R.id.btnCh4);
         switch (v.getId()) {
             case R.id.backView:
                 startActivity(new Intent(this, Game.class));
@@ -96,25 +94,25 @@ public class Characters extends AppCompatActivity implements View.OnClickListene
                 break;
 
             case R.id.btnCh1:
-                getAnswer(btn1.getText().toString());
+                if (!getAnswer(btn1.getText().toString())) minusHeart();
 
                 nextQuestion();
                 break;
 
             case R.id.btnCh2:
-                getAnswer(btn2.getText().toString());
+                if (!getAnswer(btn2.getText().toString())) minusHeart();
 
                 nextQuestion();
                 break;
 
             case R.id.btnCh3:
-                getAnswer(btn3.getText().toString());
+                if (!getAnswer(btn3.getText().toString())) minusHeart();
 
                 nextQuestion();
                 break;
 
             case R.id.btnCh4:
-                getAnswer(btn4.getText().toString());
+                if (!getAnswer(btn4.getText().toString())) minusHeart();
 
                 nextQuestion();
                 break;
@@ -125,6 +123,8 @@ public class Characters extends AppCompatActivity implements View.OnClickListene
     protected void onDestroy() {
         super.onDestroy();
         image = 0;
+        setId(-1);
+        heartCount = 3;
         imageArrCount = getRandomArray();
     }
 
@@ -133,67 +133,88 @@ public class Characters extends AppCompatActivity implements View.OnClickListene
         Dialog winDialog = new Dialog(Characters.this);
         winDialog.setContentView(R.layout.activity_win_action);
         winDialog.show();
-        final Button okButton = (Button) winDialog.findViewById(R.id.ok);
-        okButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                winDialog.hide();
-                startActivity(new Intent(Characters.this, Game.class));
-            }
+        winDialog.setCancelable(false);
+        final Button okButton = winDialog.findViewById(R.id.ok);
+        okButton.setOnClickListener(v -> {
+            winDialog.hide();
+            startActivity(new Intent(Characters.this, Game.class));
         });
     }
 
+    public void lossDialog() {
+        //Dialog window
+        Dialog lossDialog = new Dialog(Characters.this);
+        lossDialog.setContentView(R.layout.activity_loss);
+        lossDialog.show();
+        lossDialog.setCancelable(false);
+        final Button okButton = lossDialog.findViewById(R.id.ok);
+        okButton.setOnClickListener(v -> {
+            lossDialog.hide();
+            startActivity(new Intent(Characters.this, Game.class));
+        });
+    }
+
+    //Возвращает True, если ID текста кнопки совпадает с ID изображения
+    public boolean getAnswer(String str) {
+        final TextView info = findViewById(R.id.informationText);
+        boolean answer = questions.get(getId()).equals(str);
+        info.setText(answer ? R.string.correct : R.string.wrong);
+        return questions.get(getId()).equals(str);
+    }
+
+    public void minusHeart() {
+        final ImageView heart1 = findViewById(R.id.heart1);
+        final ImageView heart2 = findViewById(R.id.heart2);
+        final ImageView heart3 = findViewById(R.id.heart3);
+
+        heartCount--;
+        if (heart3.getVisibility() == View.VISIBLE) heart3.setVisibility(View.INVISIBLE);
+        else if (heart2.getVisibility() == View.VISIBLE) heart2.setVisibility(View.INVISIBLE);
+        else if (heart1.getVisibility() == View.VISIBLE) {
+            heart1.setVisibility(View.INVISIBLE);
+            lossDialog();
+        }
+    }
+
+    //Вовзращает ID изображения, вопроса, ответа в LinkedList.
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public void nextQuestion() {
-        final ImageView characterView = (ImageView) findViewById(R.id.characterView);
-        final Button btn1 = (Button) findViewById(R.id.btnCh1);
-        final Button btn2 = (Button) findViewById(R.id.btnCh2);
-        final Button btn3 = (Button) findViewById(R.id.btnCh3);
-        final Button btn4 = (Button) findViewById(R.id.btnCh4);
+        final ImageView characterView = findViewById(R.id.characterView);
+        final Button btn1 = findViewById(R.id.btnCh1);
+        final Button btn2 = findViewById(R.id.btnCh2);
+        final Button btn3 = findViewById(R.id.btnCh3);
+        final Button btn4 = findViewById(R.id.btnCh4);
 
-        Integer[] arr = getRandomArray();
-
-        btn1.setText(questions.get(arr[0]));
-        btn2.setText(questions.get(arr[1]));
-        btn3.setText(questions.get(arr[2]));
-        btn4.setText(questions.get(arr[3]));
+        if (image == images.size()) winDialog();
 
         try {
-            characterView.setImageDrawable(images.get(imageArrCount[image]));
+            //Свайп изображения
+            setId(imageArrCount[image]);
+            characterView.setImageDrawable(images.get(getId()));
             image++;
-        } catch (IndexOutOfBoundsException e) {
-            e.getStackTrace();
-            System.out.println("Done");
-            winDialog();
+        } catch (ArrayIndexOutOfBoundsException ignored) {
         }
+
+        Integer[] buttonArrCount = getRandomArrayButton(getRandomArray());
+        //Обновление кнопок
+        btn1.setText(questions.get(buttonArrCount[0]));
+        btn2.setText(questions.get(buttonArrCount[1]));
+        btn3.setText(questions.get(buttonArrCount[2]));
+        btn4.setText(questions.get(buttonArrCount[3]));
     }
 
-    public boolean getAnswer(String imageChoice) {
-        final ImageView characterView = (ImageView) findViewById(R.id.characterView);
-        final TextView info = (TextView) findViewById(R.id.informationText);
-        Integer[] keys = questions.keySet().toArray(new Integer[imageArrCount.length]);
-        String[] values = questions.values().toArray(new String[imageArrCount.length]);
-        try {
-            int i = 0;
-            for (; i < values.length; i++) {
-                if (imageChoice.equals(values[i])) break;
-            }
-            if (characterView.getDrawable().getCurrent() == images.get(keys[i])) {
-                correctAnswer = true;
-                info.setText("true!");
-                return true;
-            } else {
-                info.setText("false");
-                correctAnswer = false;
-                return false;
-            }
-        } catch (IndexOutOfBoundsException e) {
-            e.getStackTrace();
-        }
-        return false;
-    }
-
+    //Сгенерировать рандомный массив с индексами
     public Integer[] getRandomArray() {
-        Integer[] arr = new Integer[images.size()];
+        //если для изображения, то нужно все значения в произвольном порядке
+        Integer[] arr = new Integer[questions.size()];
+
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i;
         }
@@ -202,13 +223,32 @@ public class Characters extends AppCompatActivity implements View.OnClickListene
         return arr;
     }
 
+    //Сгенерировать рандомный массив с индексами
+    public Integer[] getRandomArrayButton(Integer[] arr) {
+        //если для кнопок, то нужно 4 значения с 1 верным
 
-    class Failure extends Thread {
-
-        @Override
-        public void run() {
-            super.run();
-
+        Integer[] arrForButtons = new Integer[4];
+        for (int i = 0; i < arrForButtons.length; i++) {
+            if (arr[i] == getId()) continue;
+            arrForButtons[i] = arr[i];
         }
+
+        for (int i = 0; i < arrForButtons.length; i++) {
+            if (arrForButtons[i] == null) {
+                arrForButtons[i] = getId();
+                break;
+            }
+        }
+
+        if (arrForButtons[0] == getId()) return arrForButtons;
+        else if (arrForButtons[1] == getId()) return arrForButtons;
+        else if (arrForButtons[2] == getId()) return arrForButtons;
+        else if (arrForButtons[3] == getId()) return arrForButtons;
+        else arrForButtons[0] = getId();
+
+
+        Collections.shuffle(Arrays.asList(arrForButtons));
+
+        return arrForButtons;
     }
 }
